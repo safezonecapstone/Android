0cb43b4acc05a05d4920df5be09e932baac2cf90
ABortoc <abortoc@gmail.com>
2019-03-26 13:27:07 -0400

    Added a snackbar to enable location if it is disabled. Works on fresh start

--- app/src/main/java/com/safezone/app/MainActivity.java
+++ app/src/main/java/com/safezone/app/MainActivity.java
@@ -1,17 +1,22 @@
 package com.safezone.app;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +24,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -30,6 +45,7 @@ public class MainActivity extends Activity {
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 
     //widgets
     private EditText mAddress;
@@ -47,6 +63,10 @@ public class MainActivity extends Activity {
         mAddress = (EditText) findViewById(R.id.input_search_main);
 
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showLocationBar();
+        }
 
         // Define a listener that responds to location updates
         LocationListener locationListener = new LocationListener() {
@@ -81,6 +101,67 @@ public class MainActivity extends Activity {
         if(isServicesOK()) {
             init();
         }
+    }
+
+    @SuppressLint("WrongConstant")
+    private void showLocationBar() {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.main_activity), "Location disabled", Snackbar.LENGTH_LONG)
+                .setAction("Enable", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        displayLocationSettingsRequest(getApplicationContext());
+                    }
+                });
+        snackbar.setActionTextColor(Color.WHITE);
+        snackbar.setDuration(Snackbar.LENGTH_INDEFINITE);
+
+        View snackView = snackbar.getView();
+        TextView textView = (TextView) snackView.findViewById(com.google.android.material.R.id.snackbar_text);
+        textView.setTextColor(Color.YELLOW);
+
+        snackbar.show();
+    }
+
+    private void displayLocationSettingsRequest(Context context) {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Log.i(TAG, "All location settings are satisfied.");
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+                        try {
+                            // Show the dialog by calling startResolutionForResult(), and check the result
+                            // in onActivityResult().
+                            status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.i(TAG, "PendingIntent unable to execute request.");
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+                        break;
+                }
+            }
+        });
     }
 
     private void init() {
